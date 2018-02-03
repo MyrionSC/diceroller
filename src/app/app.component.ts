a0e742013e492bde0114e7405a6b3f5efbc58476
myrionsc <marpht23@gmail.com>
2018-02-03 16:13:55 +0100

    removed >=5 and refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, ElementRef, HostListener, Renderer, ViewChild} from '@angular/core';
-declare var Fraction: any;
 
 @Component({
   selector: 'app-root',
@@ -13,14 +12,11 @@ export class AppComponent {
     diceField2 = 0;
     diceField3 = 0;
     diceField4 = 0;
-    diceField5 = 0;
     resultField1 = -1;
     resultField2 = -1;
     resultField3 = -1;
     resultField4 = -1;
-    resultField5 = -1;
     total = -1;
-    showFive = false;
 
     imageIterator = 0;
     images = [
@@ -49,7 +45,7 @@ export class AppComponent {
 
     @HostListener('window:keydown', ['$event'])
     keyboardInput(event: KeyboardEvent) {
-        if (event.code === 'Space' && event.ctrlKey === true) {
+        if (event.code === 'Enter') {
             this.rollDice();
         } else if (event.code === 'KeyB' && event.ctrlKey === true) {
             this.reset();
@@ -57,11 +53,6 @@ export class AppComponent {
     }
 
     constructor(private renderer: Renderer) {
-        console.log("lskdjf");
-        console.log(new Fraction(1, 2));
-        console.log(new Fraction(1, 2));
-
-
         setInterval(() => {
             console.log(this.imageIterator);
             this.backgroundImage = this.images[this.imageIterator++];
@@ -76,8 +67,7 @@ export class AppComponent {
         this.resultField2 = this.roll(this.diceField2, 2);
         this.resultField3 = this.roll(this.diceField3, 3);
         this.resultField4 = this.roll(this.diceField4, 4);
-        this.resultField5 = this.roll(this.diceField5, 5);
-        this.total = this.resultField1 + this.resultField2 + this.resultField3 + this.resultField4 + this.resultField5;
+        this.total = this.resultField1 + this.resultField2 + this.resultField3 + this.resultField4;
 
         this.renderer.invokeElementMethod(this.firstInput.nativeElement, 'select', []);
     }
@@ -98,12 +88,10 @@ export class AppComponent {
         this.diceField2 = 0;
         this.diceField3 = 0;
         this.diceField4 = 0;
-        this.diceField5 = 0;
         this.resultField1 = -1;
         this.resultField2 = -1;
         this.resultField3 = -1;
         this.resultField4 = -1;
-        this.resultField5 = -1;
         this.total = -1;
         this.renderer.invokeElementMethod(this.firstInput.nativeElement, 'select', []);
     }
